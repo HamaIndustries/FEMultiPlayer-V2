e06772833608d59462febe4ac0da76c842a6bce9
Eli <eliatlarge@gmail.com>
2016-03-07 07:25:04 -0600

    Create low filesize dl

--- res/music/.filenames.py
+++ res/music/.filenames.py
@@ -2,7 +2,7 @@ from os import listdir, remove
 files = [f if not f[0] == '.' else "" for f in listdir("./")]
 with open(".audionames.txt", "w+") as out:
     for i in files:
-        if i.find('.wav')<0 and not i == '':
+        if i.find('.ogg')<0 and not i == '':
             remove(i)
         else:
-            out.write(i[0:i.find(".wav")] + "\n")
+            out.write(i[0:i.find(".ogg")] + "\n")
