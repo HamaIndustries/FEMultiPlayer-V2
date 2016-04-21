5cd665b0cf738cfc92fdeccfe40d00174b6dcd6c
Elijah Mitchell <eliatlarge@gmail.com>
2016-04-20 22:57:49 -0600

    Create tool scripts

--- scripts/clearlogs.py
+++ scripts/clearlogs.py
@@ -1,14 +1,12 @@
 import os, platform
-deer = os.listdir('.')
+deer = os.listdir('..\\')
 count = 0
 for i in range(len(deer)):
 	if '.log' in deer[i]:
-		os.remove(deer[i])
+		os.remove('..\\'+deer[i])
 		print('removed '+deer[i])
 		count+=1
 print('\nremoved '+str(count)+' logs')
-
-print(platform.python_build()[0].find('v3'))
 
 if platform.python_build()[0].find('v3')>=0:
         input('press any key to finish.')
