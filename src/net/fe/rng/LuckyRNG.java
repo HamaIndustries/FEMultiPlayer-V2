8c6d001e5b0e86318f41429c87a3c06c1a391bbd
wellme <wellmey@gmail.com>
2017-05-09 10:26:28 -0400

    Added Lucky and Unlucky RNGs as a proof of concept

---
+++ src/net/fe/rng/LuckyRNG.java
@@ -0,0 +1,23 @@
+package net.fe.rng;
+
+
+/**
+ * Mimics Path of Exiles' "lucky" mechanic. Tests twice with a single random number,
+ * but only one numbers must past the test.
+ * @author wellme
+ */
+public class LuckyRNG extends RNG {
+	
+	private static final RNG RNG_PROVIDER = new SimpleRNG();
+
+	@Override
+	public boolean test(int successRate) {
+		return RNG_PROVIDER.test(successRate) || RNG_PROVIDER.test(successRate);
+	}
+
+	@Override
+	public String getName() {
+		return "Lucky";
+	}
+
+}
