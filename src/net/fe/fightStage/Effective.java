3703d833d6221b332cccdbfcac385f91df3ce95d
Raymond Dodge <git@rayrobdod.name>
2018-09-17 13:17:14 -0400

    Implement Object methods in Effective

--- src/net/fe/fightStage/Effective.java
+++ src/net/fe/fightStage/Effective.java
@@ -7,11 +7,13 @@ import net.fe.rng.RNG;
 import net.fe.unit.Unit;
 
 /**
- * Multiplies a weapon's str by a given amount
+ * Multiplies a weapon's mt by a given amount when fighting a unit with a
+ * specified other class
  * 
  * Implemented by increasing the user's mag and str by a multiple of the
  * equipped unit's weapon's might
  * 
+ * Condition: enemy class in contained in this's list of classes
  * Chance: 100%
  */
 public final class Effective extends CombatTrigger{
@@ -23,6 +25,7 @@ public final class Effective extends CombatTrigger{
 	public final List<String> classes;
 	
 	/**
+	 * The parameters match this's public fields, modulo some defensive copying
 	 */
 	public Effective(int multiplier, List<String> classes){
 		super(NO_NAME_MOD, YOUR_TURN_PRE | SHOW_IN_PREVIEW);
@@ -46,7 +49,22 @@ public final class Effective extends CombatTrigger{
 		return true;
 	}
 	
+	@Override
 	public CombatTrigger getCopy(){
 		return new Effective(this.multiplier, this.classes);
 	}
+	
+	@Override
+	protected boolean canEquals(Object other) {
+		return other instanceof Effective;
+	}
+	@Override
+	public boolean equals(Object other) {
+		return other instanceof Effective &&
+			((Effective) other).canEquals(this) &&
+			((Effective) other).multiplier == this.multiplier &&
+			((Effective) other).classes.equals(this.classes);
+	}
+	@Override public int hashCode() { return this.getName().hashCode() * 31 + multiplier * 31 + this.classes.hashCode(); }
+	@Override public String toString() { return this.getName() + " x" + multiplier + " " + this.classes.toString(); }
 }
